48ebc007ccbb948f2b4f5adb450f2b9cda1258d2
Stelgo <JTL.STEL@proton.me>
2026-05-17 23:13:36 +0300

    New code (bug fixes)
    Bug fixes.
    Saving passwords doesn't work, yet. :( sorry.
    Removed counting, if too profiles with the same key value is present it's deleted.

--- PmPy.py
+++ PmPy.py
@@ -2,14 +2,12 @@ from cryptography.fernet import Fernet, InvalidToken
 import json , time
 #Vars:
 
-try:
-    with open("CountingX_PmPy.txt", "r") as FIleC:
-    x = FileC.read()
-except FileNotFoundError:
-    x = 0    
 masterdict = {}
 tempdict = {}
-y = 0 
+y = 0
+
+Flag = True 
+
 
 KEY_PATH = "PasswordManager.key"
 # loads a key or generates a new one if a key is not found
@@ -33,69 +31,92 @@ f = Fernet(key)
 
 def NewProfile(KEY=f):
     global tempdict
-    global x
-    x += 1
     NewItemName = input("New Login Username: ")
     NewItemPasswrd = input("New Login Password: ")
     Profile = NewItemName
     PF_Password = NewItemPasswrd
     token = KEY.encrypt(PF_Password.encode('utf-8'))
     PF_Password = token
-    
-    tempdict[f"{x}{Profile}"] = PF_Password
-    with open("CountingX_PmPy.txt", "w") as FileC:
-        FileC.write(x)
+    tempdict.update({f"{Profile}" : PF_Password})
     print("New profile created.")
     return tempdict
     
-def SaveNewProfile(NewPasswords):
-  while True:
-    userinpt = input("Are you sure you want to save? (y / n)")
-    userinpt = userinpt.lower().strip()
-    if userinpt in ("y", "yes"):
-        print("Profile saved.")
-        with open("PasswordPmPy.json", "a", encoding="utf-8") as File:
-        File.write(json.dumps(new_passwords, ensure_ascii=False) + "\n")
-        time.wait(2)
-        break
-    elif userinpt in ("n" , "no"):
-        print("Aborted. (Did not save the file)")
-    else:
-        print("Wrong input try again")
-        global y
-        y += 1
-        if y == 3:
-            choise = input("Do you want to Abort? (y / n) ")
-            if choise in ("y", "yes"):
-                global y
-                y = 0
-                break
-            elif choise in ("n" , "no"):
-                global y
-                y = 0
-                pass
+def SaveNewProfile():
+    global tempdict
+    while True:
+        userinpt = input("Are you sure you want to save? (y / n)")
+        userinpt = userinpt.lower().strip()
+        if userinpt in ("y", "yes"):
+            print("Profile saved.")
+            tempRender = json.dumps(tempdict)
+            with open("PasswordPmPy.json", "a", encoding="utf-8") as File:
+                File.write(json.dumps(tempRender, ensure_ascii=False) + "\n")
+            time.wait(2)
+            break
+        elif userinpt in ("n" , "no"):
+            print("Aborted. (Did not save the file)")
+            break
+        else:
+            print("Wrong input try again")
+            global y
+            y += 1
+            if y == 3:
+                choise = input("Do you want to Abort? (y / n) ")
+                if choise in ("y", "yes"):
+                    y = 0
+                    break
+                elif choise in ("n" , "no"):
+                    y = 0
+                    pass
 
 
 def LoadStoredProfiles():
     global masterdict
-    with open("PasswordPmPy.json", "r", encoding="utf-8") as f:
-        for line in f:
-            line = line.strip()
-            if not line:
-                continue
-            obj = json.loads(line)          
-            masterdict.update(obj)        
+    try:
+        with open("PasswordPmPy.json", "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                if not line:
+                    continue
+                obj = json.loads(line)          
+                masterdict.update(obj)        
+    except FileNotFoundError:
+        with open("PasswordPmPy.json", "w", encoding="utf-8") as f:
+            f.close()
 
 
 def GetProfile():
     global masterdict
+    global tempdict
+    global f
     userinpt = input("Enter profile name: ")
     userinpt = userinpt.strip()
-    temp = masterdict.get(userinpt)
-    print(temp)
+    print(f"Saved profiles with the name: {userinpt} ")
+    try:
+        UncryptedSaved = masterdict.get(userinpt)
+        temp = f.decrypt(UncryptedSaved)
+        UncryptedSaved = temp
+        print('')
+        print(UncryptedSaved)
+    except TypeError:
+        print('')
+        print(f"No saved profiles with the  name: {userinpt} ")
+    print('')
+    print("-----------------------------------------------------")
+    print('')
+    print(f"Not saved profiles with the name: {userinpt} ")
+    try:
+        UncryptedTemp = tempdict.get(userinpt)
+        temp = f.decrypt(UncryptedTemp)
+        UncryptedTemp = temp
+        print('')
+        print(UncryptedTemp)
+    except TypeError:
+        print('')
+        print(f"No not saved profiles with the  name: {userinpt} ")
     
 def DeleteProfile():
-    userinpt = input().strip()
+    userinpt = input("Profile to be deleted: ").strip()
     try:
         global masterdict
         del masterdict[userinpt]
@@ -112,23 +133,34 @@ def DeleteProfile():
 #Load Saved profiles
 LoadStoredProfiles()
 #User input
-print("options are: SHOW, GET, DELETE, SAVE, NEW")
-CMD = input("")
-CMD = CMD.upper()
-
-if CMD == 'NEW':
-    NewProfile()
-elif CMD == 'SAVE':
-    SaveNewProfile(tempdict)
-elif CMD == 'DELETE':
-    DeleteProfile()
-elif CMD == 'GET':
-    GetProfile()
-elif CMD == 'SHOW':
-    print("Not saved profiles:")
-    print(tempdict)
+print("WARNING: If two (or more) profiles have the same name THE LATEST PROFILE CREATED WILL BE KEPT WHILE THE OTHER WILL GET DELTED")
+while Flag:
     print("----------------------------------")
-    print("Saved profiles:")
-    print (masterdict)
-
+    print("options are: SHOW, GET, DELETE, SAVE, NEW, EXIT")
+    CMD = input("> ")
+    CMD = CMD.upper().strip()
+    if CMD == 'NEW':
+        NewProfile()
+    elif CMD == 'SAVE':
+        SaveNewProfile()
+    elif CMD == 'DELETE':
+        DeleteProfile()
+    elif CMD == 'GET':
+        GetProfile()
+    elif CMD == 'SHOW':
+        print("Not saved profiles:")
+        print(tempdict)
+        print("----------------------------------")
+        print("Saved profiles:")
+        print (masterdict)
+    elif CMD == "EXIT":
+        UserInput = input("Are you sure you want to quit? ( y / n ): ")
+        if UserInput in ('yes' , 'y'):
+            print("Exiting PmPy, all unsaved profiles will be deleted.")
+            break
+            quit()
+        else:
+            print("Did not exit.")
+    else:
+        print("Not valid command.")
 
